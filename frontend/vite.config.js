de2223ff5963275d42cce3b2ec7d5dbc0a731005
Indhuja2109 <indhujav.cse2022@citchennai.net>
2024-12-12 10:52:43 +0530

    updated routes - vercel.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/travel-story-app/', // Adjust to your subpath if necessary
+});
