94cc2631a67e992b0752a128b58771641d77f15d
Indhuja2109 <indhujav.cse2022@citchennai.net>
2024-12-12 11:00:04 +0530

    base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/travel-story-app/', // Adjust to your subpath if necessary
+  base: '/',  // Set this to the correct base path if your app is not at the root
+  plugins: [react()]
 });
